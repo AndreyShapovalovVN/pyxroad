e77e80d2e4a3ffe9eb6c80f3804291fc2d208b57
AndreyShapovalovVN <a147@ukr.net>
2020-02-06 14:23:46 +0200

    Refactoring

--- XRoad/Client.py
+++ XRoad/Client.py
@@ -33,9 +33,13 @@ class XRoadPlugin(zeep.Plugin):
         el = header.find('{http://x-road.eu/xsd/xroad.xsd}id')
         el.text = uuid.uuid4().hex
 
+        el = header.find('{http://x-road.eu/xsd/xroad.xsd}protocolVersion')
+        el.text = '4.0'
+
         service = header.find('{http://x-road.eu/xsd/xroad.xsd}service')
         el = service.find('{http://x-road.eu/xsd/identifiers}serviceCode')
-        el.text = operation.name
+        if not el.text:
+            el.text = operation.name
 
         # For some reason, zeep insists on adding WSA elements to the
         # header. This will only confuse some servers, so remove the
@@ -49,58 +53,52 @@ class XRoadPlugin(zeep.Plugin):
 
 
 class Client(zeep.Client):
-    HEADER = {
-        'client': {
-            'objectType': 'SUBSYSTEM',
-            'xRoadInstance': None,
-            'memberClass': None,
-            'memberCode': None,
-            'subsystemCode': None,
-        },
-        'service': {
-            'objectType': "SERVICE",
-            'xRoadInstance': None,
-            'memberClass': None,
-            'memberCode': None,
-            'subsystemCode': None,
-            'serviceCode': None,
-            'serviceVersion': None,
-        },
-        'userId': None,
-        'id': None,
-        'protocolVersion': None
-    }
+    HEADER = {}
 
-    def __init__(self, wsdl, client=None, service=None,
+    def __init__(self, wsdl,
+                 client=None, service=None,
                  protocolVersion=4.0,
-                 userId='?', id='?',
+                 userId='?',
+                 id='?',
                  *args, **kwargs):
         self.security_server_url = wsdl
 
         client = client.split('/')
-        assert len(client) == 4
-        self.HEADER['client']['xRoadInstance'] = client[0]
-        self.HEADER['client']['memberClass'] = client[1]
-        self.HEADER['client']['memberCode'] = client[2]
-        self.HEADER['client']['subsystemCode'] = client[3]
-
         service = service.split('/')
-        self.HEADER['service']['xRoadInstance'] = service[0]
-        self.HEADER['service']['memberClass'] = service[1]
-        self.HEADER['service']['memberCode'] = service[2]
-        self.HEADER['service']['subsystemCode'] = service[3]
-        self.HEADER['service']['serviceCode'] = service[4]
-        if len(service) > 5:
-            self.HEADER['service']['serviceVersion'] = service[5]
+        service.append(None)
+        assert len(client) == 4
+        assert len(service) in (5, 6)
 
-        self.HEADER['protocolVersion'] = protocolVersion
-        self.HEADER['userId'] = userId
-        self.HEADER['id'] = id
+        self.HEADER.update(
+            {
+                'client': {
+                    'xRoadInstance': client[0],
+                    'memberClass': client[1],
+                    'memberCode': client[2],
+                    'subsystemCode': client[3],
+                },
+                'service': {
+                    'xRoadInstance': service[0],
+                    'memberClass': service[1],
+                    'memberCode': service[2],
+                    'subsystemCode': service[3],
+                    'serviceCode': service[4],
+                    'serviceVersion': service[5],
+                },
+                'protocolVersion': protocolVersion,
+                'userId': userId,
+                'id': id,
+            }
+        )
 
         if '/wsdl' not in wsdl:
-            wsdl = self._get_url_wsdl(wsdl)
+            wsdl = requests.Request(
+                'GET',
+                wsdl + '/wsdl',
+                params=self.HEADER['service']
+            ).prepare().url
 
-        plugins = kwargs.get('plugins', [])
+        plugins = kwargs.get('plugins') or []
         plugins.append(XRoadPlugin(self))
         kwargs['plugins'] = plugins
 
@@ -110,15 +108,3 @@ class Client(zeep.Client):
         self.set_ns_prefix('iden', "http://x-road.eu/xsd/identifiers")
 
         self.set_default_soapheaders(self.HEADER)
-
-    def _get_url_wsdl(self, url):
-        params = {
-            'xRoadInstance': self.HEADER['service'].get('xRoadInstance'),
-            'memberClass': self.HEADER['service'].get('memberClass'),
-            'memberCode': self.HEADER['service'].get('memberCode'),
-            'serviceCode': self.HEADER['service'].get('serviceCode'),
-            'subsystemCode': self.HEADER['service'].get('subsystemCode'),
-            'version': self.HEADER['service'].get('serviceVersion')
-        }
-        return requests.Request(
-            'GET', url + '/wsdl', params=params).prepare().url
--- XRoad/__init__.py
+++ XRoad/__init__.py
@@ -1,3 +1,3 @@
 from XRoad.Client import Client
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
