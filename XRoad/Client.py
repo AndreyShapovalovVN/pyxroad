551e40aa8c6f4a79dc255804c86d588eed051826
AndreyShapovalovVN <a147@ukr.net>
2021-01-02 22:37:56 +0200

    PlPy Cache

--- XRoad/Client.py
+++ XRoad/Client.py
@@ -1,8 +1,12 @@
+import datetime
 import logging
+import sys
+import threading
 import uuid
 from urllib import parse
 
 from zeep import Plugin, Client
+from zeep.cache import _is_expired, VersionedCacheBase
 from zeep.exceptions import Fault
 from zeep.helpers import serialize_object
 
@@ -206,6 +210,46 @@ class XClient(Client):
     def get_input_elements(self):
         input_element = self.wsdl_elements('input', report=False)
         return input_element.get('input')
+
+
+class PlCache(VersionedCacheBase):
+    _version = "1"
+
+    def __init__(self, timeout=3600):
+        if 'plpy' not in sys.modules:
+            raise RuntimeError("Class only Pl/Python")
+
+        self._lock = threading.RLock()
+        self._timeout = timeout
+
+        plpy.execute(
+            """
+                CREATE TABLE IF NOT EXISTS request
+                (created timestamp, url text, content text)
+            """
+        )
+        plpy.commit()
+
+    def add(self, url, content):
+        _logger.debug("Caching contents of %s", url)
+        data = self._encode_data(content)
+
+        plpy.execute("DELETE FROM request WHERE url = ?", (url,))
+        plpy.execute(
+            "INSERT INTO request (created, url, content) VALUES (?, ?, ?)",
+            (datetime.datetime.utcnow(), url, data),
+        )
+        plpy.commit()
+
+    def get(self, url):
+        rows = plpy.execute("SELECT created, content FROM request WHERE url=?",
+                            (url,))
+        if rows:
+            created, data = rows[0]
+            if not _is_expired(created, self._timeout):
+                _logger.debug("Cache HIT for %s", url)
+                return self._decode_data(data)
+        _logger.debug("Cache MISS for %s", url)
 
 
 def _get_wsdl_url(host, service):
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='XRoad',
-    version='1.2.8',
+    version='1.2.9',
     description='X-Road (Trembita) client',
     packages=find_packages(),
     include_package_data=True,
